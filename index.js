8d3cd1d33dfbd6424fad65fd2975657dd87590e6
Tyrone Spears <Spearstyrone@gmail.com>
2023-09-14 17:08:47 -0500

    addsphase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10);
+function writeCards(names, eventName) {
+    const cards = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        cards.push(message);
+    }
+
+    return cards;
+}
